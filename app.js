1e8f1f834efaf37a6075900959ea7e1592bf75d6
Veysel559 <veyselmaras2@gmail.com>
2023-07-03 00:26:52 +0300

    return added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 let items = ["Buy food", "cook food", "eat food"];
@@ -10,16 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDay();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
@@ -38,11 +30,15 @@ app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.post("/work", function (req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
+app.get("/about", function (req, res) {
+  res.render("about");
 });
+
+// app.post("/work", function (req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
 // Setting up the server at port 3000
 app.listen(3000, function () {
   console.log("server running");
