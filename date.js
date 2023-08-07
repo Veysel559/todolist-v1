1a269b183b4ea847cd29c3888bd3e0d56d0d0e65
Veysel559 <veyselmaras2@gmail.com>
2023-08-07 16:21:42 +0300

    ver2

--- date.js
+++ date.js
@@ -1,23 +1,27 @@
-exports.getDate = function () {
-  let today = new Date();
+//jshint esversion:6
 
-  let options = {
+exports.getDate = function() {
+
+  const today = new Date();
+
+  const options = {
     weekday: "long",
     day: "numeric",
-    month: "long",
+    month: "long"
   };
 
   return today.toLocaleDateString("en-US", options);
+
 };
 
 exports.getDay = function () {
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
   };
 
   return today.toLocaleDateString("en-US", options);
-};
 
-console.log(module.exports);
+};
